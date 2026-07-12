10607b92fc47c336378a7756187f5e5982c3265f
Moinul-Islam-71 <smmoinulislam710@gmail.com>
2026-07-12 22:00:18 +0600

    controls the if-else statement using ternary opeartor instead of if-else block

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let clicked = false
 
 const displayDate = () => {
-    if(!clicked) {
-        document.getElementById('dateShow').innerHTML = Date()
+    (!clicked) ? (
+        
+        document.getElementById('dateShow').innerHTML = Date(),
         document.getElementById('btnDate').innerText = "Hide Date"
-    }
     
-    else {
-        document.getElementById('dateShow').innerHTML = ""
+    ) : (
+        document.getElementById('dateShow').innerHTML = "",
         document.getElementById('btnDate').innerText = "Click me to display Date and Time."
-    }
+    )
 
     clicked = !clicked;
 }
